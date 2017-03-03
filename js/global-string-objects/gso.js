ecb972517e9b7e7705b53d6cb19341f73ae4f0f3
Tron <wykydtronik@gmail.com>
2017-03-03 01:56:58 -0800

    document, global, and string objects

---
+++ js/global-string-objects/gso.js
@@ -0,0 +1,8 @@
+var saying = 'Home sweet home ';
+
+document.getElementById('length').innerHTML = "Length: " + saying.length;
+document.getElementById('toUpperCase').innerHTML = "toUpper: " + saying.toUpperCase();
+document.getElementById('toLowerCase').innerHTML = "toLower: " + saying.toLowerCase();
+document.getElementById('charAt').innerHTML = "charAt: " + saying.charAt(12);
+document.getElementById('indexOf').innerHTML = "indexOf: " + saying.indexOf('ee');
+document.getElementById('lastIndexOf').innerHTML = "lastIndexOf: " + saying.lastIndexOf('e');
