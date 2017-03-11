738e1f7b14b21a316316b93c24e963175a9cbbb2
Tron <wykydtronik@gmail.com>
2017-03-11 11:38:51 -0800

    example hotel

---
+++ js/chapter3/example-hotel/js/example.js
@@ -0,0 +1,21 @@
+(function() {
+  var hotel = {
+    name:'Park',
+    roomRate: 240,
+    discount: 15,
+    offerPrice: function() {
+      var offerRate = this.roomRate * ((100 - this.discount) / 100);
+      return offerRate;
+    }
+  };
+
+  var hotelName, roomRate, specialRate;
+
+  hotelName = document.getElementById('hotelName');
+  roomRate = document.getElementById('roomRate');
+  specialRate = document.getElementById('specialRate');
+
+  hotelName.textContent = hotel.name;
+  roomRate.textContent = '$' + hotel.roomRate.toFixed(2);
+  specialRate.textContent = '$' + hotel.offerPrice();
+}());
