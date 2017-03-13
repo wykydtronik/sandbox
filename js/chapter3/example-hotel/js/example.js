454731cd117346111a99ebcdcb1b17a78ab7afaa
Tron <wykydtronik@gmail.com>
2017-03-12 21:46:00 -0700

    example.js changes

--- js/chapter3/example-hotel/js/example.js
+++ js/chapter3/example-hotel/js/example.js
@@ -18,4 +18,34 @@
   hotelName.textContent = hotel.name;
   roomRate.textContent = '$' + hotel.roomRate.toFixed(2);
   specialRate.textContent = '$' + hotel.offerPrice();
+
+  // Calculate and write out the expiry details for the offers
+  var expiryMsg;
+  var today;
+  var elEnds;
+
+  function offerExpires(today) {
+    // variables
+    var weekFromToday, day, date, month, year, dayNames, monthNames;
+    // add 7 day time
+    weekFromToday = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
+    //Create arrays to hold names of days / monthNames
+    dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    // Collect the parts of the date to show on the page
+    day = dayNames[weekFromToday.getDay()];
+    date = weekFromToday.getDate();
+    month = monthNames[weekFromToday.getMonth()];
+    year = weekFromToday.getFullYear();
+    // Create msg
+    expiryMsg = 'Offer expires next ';
+    expiryMsg += day + ' <br />(' + date + ' ' + month + ' ' + year + ')';
+    return expiryMsg;
+  }
+
+  today = new Date();
+  elEnds = document.getElementById('offerEnds');
+  elEnds.innerHTML = offerExpires(today);
+
+  // Finish
 }());
