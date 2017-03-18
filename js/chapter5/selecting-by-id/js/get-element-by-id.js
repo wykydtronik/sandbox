63d9921350895ee997955e0dfb11dfd29b2bac25
Gregory Fisher <hughes+k@nissanpacific.com>
2017-03-17 23:09:19 -0700

    selecting elements using id attributes

---
+++ js/chapter5/selecting-by-id/js/get-element-by-id.js
@@ -0,0 +1,10 @@
+// Select the element and store it in a variable
+
+var el = document.getElementById('one');
+
+// Change the value of the class attribute
+
+function coolDown() {
+  // just using onmouseover to trigger the function to see the difference in an interactive way
+  el.className = 'cool';
+}
