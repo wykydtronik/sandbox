a1cc8e16f6f7432fa4f3deb3768539605404a9d7
Tron <wykydtronik@gmail.com>
2017-03-20 01:16:48 -0700

    update text and markup

--- js/chapter5/changing-text-node/js/text-node.js
+++
@@ -1,7 +0,0 @@
-var itemTwo = document.getElementById('two');
-var elText = itemTwo.firstChild.nodeValue;
-
-function changeIt() {
-  elText = elText.replace('pine nuts', 'kale');
-  itemTwo.firstChild.nodeValue = elText;
-}
