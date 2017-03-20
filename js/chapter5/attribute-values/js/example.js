71059aa02074066fa0cd86057215bbf8573218d6
Tron <wykydtronik@gmail.com>
2017-03-20 12:32:59 -0700

    attribute values

---
+++ js/chapter5/attribute-values/js/example.js
@@ -0,0 +1,10 @@
+var firstItem = document.getElementById('one');
+
+function changeIt() {
+  if (firstItem.hasAttribute('class')) {
+    var attr = firstItem.getAttribute('class');
+
+    var el = document.getElementById('scriptResults');
+    el.innerHTML = '<p>The first item has a class name: ' + attr + '</p>'
+  }
+}
