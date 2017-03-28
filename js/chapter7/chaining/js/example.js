47632bb872e3ebe5e00851d859afe6841eefe233
Tron <wykydtronik@gmail.com>
2017-03-27 22:04:04 -0700

    fade in chaining

--- js/chapter7/chaining/js/example.js
+++ js/chapter7/chaining/js/example.js
@@ -1,7 +1,7 @@
 
 function dojQuery() {
-  $('li em').addClass('seasonal');
-  $('li.hot').addClass('favorite');
+  $('li em').addClass('seasonal').hide().delay(500).fadeIn(1400);
+  $('li.hot').addClass('favorite').hide().delay(500).fadeIn(1400);
   $('li[id!="one"]').hide().delay(500).fadeIn(1400);
 }
 
