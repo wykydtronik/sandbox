715e7395a8d849b6294debd4a82c2b5c3552f65a
Tron <wykydtronik@gmail.com>
2017-03-29 15:54:25 -0700

    adding new content

---
+++ js/chapter7/adding-new-content/js/example.js
@@ -0,0 +1,11 @@
+
+function dojQuery() {
+  $(function() {
+    $('ul').before('<p class="notice">Just updated</p>');
+    $('li.hot').prepend('+ ');
+    var $newListItem = $('<li><em>gluten-free</em> soy sauce</li>');
+    $('li:last').after($newListItem);
+  });
+}
+
+document.addEventListener("click", dojQuery);
