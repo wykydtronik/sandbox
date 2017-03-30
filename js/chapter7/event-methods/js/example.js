6137306039a119d4dc00300c1f3a2999d103b27e
Tron <wykydtronik@gmail.com>
2017-03-30 15:31:56 -0700

    jquery event methods

---
+++ js/chapter7/event-methods/js/example.js
@@ -0,0 +1,23 @@
+
+function dojQuery() {
+  $(function() {
+
+  });
+}
+
+// document.addEventListener("click", dojQuery);
+
+$(function() {
+  var ids = '';
+  var $listItems = $('li');
+
+  $listItems.on('mouseover click', function() {
+    ids = this.id;
+    $listItems.children('span').remove();
+    $(this).append(' <span class="priority">' + ids + '</span>');
+  });
+
+  $listItems.on('mouseout', function() {
+    $(this).children('span').remove();
+  });
+});
