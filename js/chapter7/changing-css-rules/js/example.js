9a40d9858627acabc87259f75e1e9107a01edd9b
Tron <wykydtronik@gmail.com>
2017-03-29 22:39:26 -0700

    changing css rules

--- js/chapter7/changing-css-rules/js/example.js
+++ js/chapter7/changing-css-rules/js/example.js
@@ -1,9 +1,16 @@
 
 function dojQuery() {
   $(function() {
-    $('li#three').removeClass('hot');
-    $('li.hot').addClass('favorite');
-    $('ul').attr('id', 'group');
+    var backgroundColor = $('li').css('background-color');
+    // alert(backgroundColor);
+    $('ul').append('<p>Background color was: ' + backgroundColor + '</p>');
+    $('li').css({
+      'background-color': '#c5a996',
+      'border': '1px solid #fff',
+      'color': '#000',
+      'font-family': 'Georgia',
+      'padding-left': '+=75'
+    });
   });
 }
 
