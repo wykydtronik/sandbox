13f60c5ee7aee16b0818ca8622936f22e380ee9d
Tron <wykydtronik@gmail.com>
2017-03-31 22:13:47 -0700

    filters

---
+++ js/chapter7/filters-in-use/js/example.js
@@ -0,0 +1,18 @@
+
+function dojQuery() {
+  var $listItems = $('li');
+  $listItems.filter('.hot:last').removeClass('hot');
+  $('li:not(.hot)').addClass('cool');
+  $listItems.has('em').addClass('complete');
+
+  $listItems.each(function() {
+    var $this = $(this);
+    if ($this.is('.hot')) {
+      $this.prepend('Priority item: ');
+    }
+  });
+
+  $('li:contains("honey")').append(' (local)');
+}
+
+document.addEventListener("click", dojQuery);
