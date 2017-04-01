8ca497262716c195204c18ed387561eb0fef0c10
Tron <wykydtronik@gmail.com>
2017-03-31 19:21:23 -0700

    basic effect

---
+++ js/chapter7/basic-effects/js/example.js
@@ -0,0 +1,19 @@
+
+function dojQuery() {
+  $(function() {
+
+  });
+}
+
+// document.addEventListener("click", dojQuery);
+
+$(function() {
+  $('h2').hide().slideDown();
+  var $li = $('li');
+  $li.hide().each(function(index) {
+    $(this).delay(700 * index).fadeIn(700);
+  });
+  $li.on('click', function() {
+    $(this).fadeOut(700);
+  });
+});
