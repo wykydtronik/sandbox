6c221b3cb75ee6e605308742b67888ea8a5f9781
Tron <wykydtronik@gmail.com>
2017-03-31 19:57:53 -0700

    traversing

---
+++ js/chapter7/traversing/js/example.js
@@ -0,0 +1,22 @@
+
+function dojQuery() {
+  $(function() {
+
+  });
+}
+
+// document.addEventListener("click", dojQuery);
+
+$(function() {
+  var $h2 = $('h2');
+  $('ul').hide();
+  $h2.append('<a class="show">show</a>');
+
+  $h2.on('click', function() {
+    $h2.next()
+      .fadeIn(500)
+      .children('.hot')
+      .addClass('complete');
+    $h2.find('a').fadeOut();
+  });
+});
