9bf6e184a9cf25614d694a873ad97d56caf13a94
TЯON|K <wykydtronik@gmail.com>
2017-04-01 22:48:01 -0700

    cut copy paste

---
+++ js/chapter7/cut-copy-paste/js/example.js
@@ -0,0 +1,13 @@
+function dojQuery() {
+  $(function() {
+      var $p = $('p');
+      var $clonedQuote = $p.clone();
+      $p.remove();
+      $clonedQuote.insertAfter('h2');
+
+      var $moveItem = $('#one').detach();
+      $moveItem.appendTo('ul');
+  });
+}
+
+document.addEventListener("click", dojQuery);
