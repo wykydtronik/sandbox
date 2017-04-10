db41e44e596be940ca7447446033264cf9717a82
Wykydtronik <wykydtronik@Wykydtroniks-iMac.local>
2017-04-10 16:08:50 -0700

    jsonp

---
+++ js/chapter8/using-jsonp/js/example.js
@@ -0,0 +1,34 @@
+function showEvents(data) {
+  var newContent = '';
+
+  for (var i = 0; i < data.events.length; i++) {
+    newContent += '<div class="event">';
+    newContent += '<img src="' + data.events[i].map + '"';
+    newContent += ' alt="' + data.events[i].location + '" />';
+    newContent += '<p><b>' + data.events[i].location + '</b><br>';
+    newContent += data.events[i].date + '</p>';
+    newContent += '</div>';
+  }
+
+  document.getElementById('content').innerHTML = newContent;
+}
+
+showEvents ({
+  "events": [
+    {
+      "location": "San Francisco, CA",
+      "date": "May 1",
+      "map": "img/map-ca.png"
+    },
+    {
+      "location": "Austin, TX",
+      "date": "May 15",
+      "map": "img/map-tx.png"
+    },
+    {
+      "location": "New York, NY",
+      "date": "May 30",
+      "map": "img/map-ny.png"
+    }
+  ]
+});
